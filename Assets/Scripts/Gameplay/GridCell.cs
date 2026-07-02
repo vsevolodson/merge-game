fc24452f8ee15ecf5e440e1ecdbfbae723ae6580
con <son.sev.06@gmail.com>
2026-07-02 15:49:21 +1000

    fix: перепутан местами код модели и контроллера клеток для сетки

--- Assets/Scripts/Gameplay/GridCell.cs
+++ Assets/Scripts/Gameplay/GridCell.cs
@@ -4,12 +4,5 @@ namespace Gameplay
 {
     public class GridCell : MonoBehaviour
     {
-        [Header("Grid Settings")]
-        [SerializeField] private int rows = 5;
-        [SerializeField] private int columns = 5;
-
-        [Header("References")]
-        [SerializeField] private GridCell cellPrefab;
-
     }
 }
--- Assets/Scripts/Gameplay/GridController.cs
+++ Assets/Scripts/Gameplay/GridController.cs
@@ -4,6 +4,27 @@ namespace Gameplay
 {    
     public class GridController : MonoBehaviour
     {
-        
+        [Header("Grid Settings")]
+        [SerializeField] private int rows = 5;
+        [SerializeField] private int columns = 5;
+
+        [Header("References")]
+        [SerializeField] private GridCell cellPrefab;
+
+        private void Start()
+        {
+            GenerateGrid();
+        }
+
+        private void GenerateGrid()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    Instantiate(cellPrefab, transform);
+                }
+            }
+        }
     }
 }
